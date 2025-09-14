2ed16d7bfec97ea01e1cc6733ae46b7d1fd8dd4a
rupisaini123 <47219093+rupisaini123@users.noreply.github.com>
2025-09-14 16:11:56 -0600

    added video.js

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,8 @@
-import { Component, signal } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, signal, ViewChild} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import videojs from 'video.js';
+import Player from 'video.js/dist/types/player';
+
 
 @Component({
   selector: 'app-root',
@@ -7,6 +10,26 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit, AfterViewInit{
   protected readonly title = signal('streamer');
+
+  @ViewChild('target', {static: true}) target!: ElementRef;
+  player!:  Player;
+
+  ngAfterViewInit() {
+    this.player = videojs(this.target.nativeElement,
+      {
+        fluid: true,
+        sources: [
+          {
+            src: 'https://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8'
+          }
+        ]
+      }
+    );
+  }
+
+  ngOnInit() {
+
+  }
 }
